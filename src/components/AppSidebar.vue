20f2d8011e914e5462333845372da058b45a05bd
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-03-13 12:09:41 +0800

    feat: Added versioning display

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -4,6 +4,7 @@ import { useThemeStore } from '@/stores/theme'
 import { RouterLink } from 'vue-router'
 
 const themeStore = useThemeStore()
+const version = __APP_VERSION__
 
 onMounted(() => {
   themeStore.initSystemListener()
@@ -13,48 +14,54 @@ onMounted(() => {
 <template>
   <div class="flex flex-col">
     <!-- Header Title -->
-    <div class="flex justify-between">
-      <div class="text-2xl font-bold tracking-tight">
-        <h1 class="text-accent">DevKit</h1>
+    <div>
+      <div class="flex justify-between">
+        <div class="text-2xl font-bold tracking-tight">
+          <h1 class="text-accent">DevKit</h1>
+        </div>
+
+        <button
+          @click="themeStore.toggleTheme"
+          class="cursor-pointer rounded-full p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
+        >
+          <svg
+            v-if="themeStore.isDark"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="4" />
+            <path
+              d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41"
+            />
+          </svg>
+
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+          </svg>
+        </button>
       </div>
 
-      <button
-        @click="themeStore.toggleTheme"
-        class="cursor-pointer rounded-full p-2 hover:bg-gray-200 dark:hover:bg-gray-700"
-      >
-        <svg
-          v-if="themeStore.isDark"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="12" cy="12" r="4" />
-          <path
-            d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M17.66 17.66l1.41 1.41M2 12h2M20 12h2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41"
-          />
-        </svg>
-
-        <svg
-          v-else
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
-        </svg>
-      </button>
+      <p class="text-muted-foreground bg-surface w-12 rounded-lg px-2 text-sm">
+        {{ version }}
+      </p>
     </div>
 
     <!-- Navigation -->
--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import { version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    tailwindcss()
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(version),
   },
 })
